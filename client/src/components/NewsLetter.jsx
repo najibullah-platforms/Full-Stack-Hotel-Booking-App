8667f5e0e5e4aeb8576516e8a520c10b85b53d26
najibullah-platforms <najibullah.platforms@gmail.com>
2025-11-06 12:39:22 +0500

    add new page for room

--- client/src/components/NewsLetter.jsx
+++ client/src/components/NewsLetter.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import Title from './Title'
+
 
 const NewsLetter = () => {
   return (
             <div className="flex flex-col items-center  max-w-5xl lg:w-full rounded-2xl px-4 py-12 md:py-16 mx-2 lg:mx-auto my-30 bg-gray-900 text-white">
             
+            <Title title="stay inspired" subTitle="join newsletter and be tha to new search bar for sites" />
+
             <div className="flex flex-col md:flex-row items-center justify-center gap-4 mt-6">
                 <input type="text" className="bg-white/10 px-4 py-2.5 border border-white/20 rounded outline-none max-w-66 w-full" placeholder="Enter your email" />
                 <button className="flex items-center justify-center gap-2 group bg-black px-4 md:px-7 py-2.5 rounded active:scale-95 transition-all">Subscribe
