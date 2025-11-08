6749c96d2d06f53f7f584c892e8a1b97abe8740e
najibullah-platforms <najibullah.platforms@gmail.com>
2025-11-09 00:07:00 +0500

    add now pages for

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,11 @@ import Footer from './components/Footer';
 import AllRooms from './pages/AllRooms';
 import RoomsDetails from './pages/RoomsDetails';
 import MyBooking from './pages/MyBooking';
+import HotelReg from './components/HotelReg';
+import Layout from './pages/hotelOwner/Layout';
+import Dashboard from './pages/hotelOwner/Dashboard';
+import AddRoom from './pages/hotelOwner/AddRoom';
+import ListRoom from './pages/hotelOwner/ListRoom';
 
 const App = () => {
 
@@ -14,12 +19,18 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath && <Navbar />}
+      { false && <HotelReg />}
       <div className='min-h-[70vh]'>
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/rooms' element={<AllRooms/>} />
           <Route path='/rooms/:id' element={<RoomsDetails/>} />
           <Route path='/my-booking' element={<MyBooking/>} />
+          <Route path='/owner' element={<Layout/>}>
+            <Route index element={<Dashboard />} />
+            <Route path='add-room' element={<AddRoom />} />
+            <Route path='list-room' element={<ListRoom />} />
+          </Route>
         </Routes>
       </div>
       <Footer />
---
+++ client/src/pages/hotelOwner/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from '../../components/Title'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Title align='left' font='outfit' title='Dashboard' subTitle='simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrysimply em Ipsum has been the industry' />
+    </div>
+  )
+}
+
+export default Dashboard
